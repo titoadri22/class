06909e901db8db589ff788400a16f77bc849ab56
titoadri22 <adreescutia@gmail.com>
2026-01-13 12:32:08 +0100

    Create 01-fundamentos.js

---
+++ 01-fundamentos.js
@@ -0,0 +1,20 @@
+//explicacion arrays JavaScript
+const numeros = [1,2,3]
+
+numeros[0] //numero 1 
+numeros[1] //numero 2 
+numeros[2] //numero 3
+
+const texto = ['hola', 'Js']
+
+texto[0] // hola
+texto[1] // Js
+
+// si creas una const position
+const position = 0
+//puedes elegir la posicion del array con esta const
+
+texto[position] // hola
+
+//para cambiar datos de la array
+numeros[1] = 12 // se cambiara el 2(posicion 1) por el 12
